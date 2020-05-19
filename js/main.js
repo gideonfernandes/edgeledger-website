472573373cbb5156037f211bf9ee128cf84d0bce
Gideon Fernandes <gide10062014@gmail.com>
2020-05-18 22:58:52 -0300

    Finalizes the styling

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ document.addEventListener('scroll', () => {
 		document.querySelector('header #navbar').style.opacity = 1;
 	}
 });
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 70
+      },
+      800
+    );
+  }
+});
